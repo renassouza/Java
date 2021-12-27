abed107fcca2b6db5bdbeb43d830913c2693f23c
Renata Souza <renataoliveira1497@gmail.com>
2021-12-27 09:56:49 -0300

    Add files via upload

---
+++ banco/Banco.java
@@ -0,0 +1,18 @@
+package banco;
+
+import modelos.Conta;
+
+public class Banco {
+
+	public static void main(String[] args) {
+		Conta a = new Conta("Conta 1");
+		Conta b = new Conta ("conat 2");
+		System.out.println(a.toString());
+		System.out.println(b.toString());
+		a.depositar(200);
+		System.out.println(a.toString());
+		a.sacar(300);
+		
+	}
+
+}
